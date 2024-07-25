86d1a56102f3cffa3de290b926826e89e3ec8506
tpaknok <tpaknok@gmail.com>
2024-07-25 16:47:47 -0400

    do Satterthwaitee estimations...and BEF simulate discard data failed to converge

--- R/BEF_simulate.R
+++ R/BEF_simulate.R
@@ -1,6 +1,7 @@
 BEF_simulate <- function(comm,
                          VCV_sp = NULL,
                          scale=1,
+                         spaMM_formula,
                          b1=0,
                          signals_X="phy_cor",
                          signals_Y = T,
@@ -11,62 +12,120 @@ BEF_simulate <- function(comm,
                          x_sd = 1,
                          noise_mean = 0,
                          noise_sd = 0,
-                         lambda_true=1,
-                         sim=500,
-                         seed=1000) {
+                         lambda_true=1) {
+                         #seed=1000) {
 
-  library(MASS)
-  library(vegan)
-  set.seed(seed)
+  # library(MASS)
+  # library(vegan)
+  # set.seed(seed)
+  #
+  # sim_all <- VCV_sp_list <- VCV_true_list <- list()
+  #
+  # for (i in 1:sim) {
+  #   if(is.null(VCV_sp)){
+  #     tree <- pbtree(n=ncol(comm))
+  #     tree$tip.label <- colnames(comm)
+  #
+  #     vcv <- vcv(tree)
+  #     VCV_sp_list[[i]] <- vcv
+  #     vcv_true <- vcv*lambda_true
+  #     diag(vcv_true) <- diag(vcv)
+  #     } else {
+  #     VCV_sp_list[[i]] <- VCV_sp
+  #     vcv_true <- VCV_sp*lambda_true
+  #     diag(vcv_true) <- diag(VCV_sp)
+  #   }
+  #
+  #
+  #   VCV_true_list[[i]] <- vcv_true
+  #   C_true <- get_comm_pair_r(comm,vcv_true)
+  #
+  #   if (signals_X == "phy_cor") {
+  #   #x <- t(chol(C_true)) %*% rnorm(nrow(comm),x_mean,x_sd)
+  #     x <- mvrnorm(1,rep(0,nrow(C_true)),C_true)
+  #   }
+  #
+  #   if (signals_X == "sr") {
+  #     x <- specnumber(comm)
+  #   }
+  #
+  #   if (signals_X == "no_phy_cor") {
+  #     x <- rnorm(nrow(comm),x_mean,x_sd)
+  #   }
+  #
+  #   if (signals_Y == T) {
+  #   #y <- t(chol(C_true)) %*% rnorm(nrow(comm),y_mean,y_sd)
+  #     y <- intercept+b1*x+mvrnorm(1,rep(0,nrow(C_true)),C_true)+rnorm(nrow(comm),noise_mean,noise_sd)
+  #   } else {
+  #     y <- intercept+b1*x+rnorm(nrow(comm),y_mean,y_sd)+rnorm(nrow(comm),noise_mean,noise_sd)
+  #   }
+  #
+  #   sim_all[[i]] <- data.frame(y=y,x=x)
+  # }
+  # require(parallel)
+  #
+  # .simulate_data <- function(comm,
+  #                            VCV_sp,
+  #                            scale,
+  #                            b1,
+  #                            signals_X,
+  #                            signals_Y,
+  #                            intercept,
+  #                            y_mean,
+  #                            y_sd,
+  #                            x_mean,
+  #                            x_sd,
+  #                            noise_mean,
+  #                            noise_sd,
+  #                            lambda_true,
+  #                            force.PD = F) {
+    library(MASS)
+    library(vegan)
+    library(CPR)
+    library(ape)
+    library(phytools)
 
-  sim_all <- VCV_sp_list <- VCV_true_list <- list()
+    conv <- c(0,0,0,0)
 
-  for (i in 1:sim) {
+    while (length(unique(conv)) != 1 | unique(conv) == 0) {
+
     if(is.null(VCV_sp)){
       tree <- pbtree(n=ncol(comm))
       tree$tip.label <- colnames(comm)
 
       vcv <- vcv(tree)
-      VCV_sp_list[[i]] <- vcv
       vcv_true <- vcv*lambda_true
       diag(vcv_true) <- diag(vcv)
-      } else {
-      VCV_sp_list[[i]] <- VCV_sp
+    } else {
       vcv_true <- VCV_sp*lambda_true
       diag(vcv_true) <- diag(VCV_sp)
     }
 
+    C_true <- get_comm_pair_r(comm,vcv_true,force.PD=F)$covM
+    x <- mvrnorm(1,rep(0,nrow(C_true)),C_true)
+    y <- mvrnorm(1,rep(0,nrow(C_true)),C_true)+rnorm(nrow(comm),0,1)
+    data <- data.frame(y=y,x=x,comp_id=as.character(1:nrow(comm)))
 
-    VCV_true_list[[i]] <- vcv_true
-    C_true <- get_comm_pair_r(comm,vcv_true)
+    sim_data <- list(data=data,
+                     sim_phy=vcv,
+                     true_phy=vcv_true)
 
-    if (signals_X == "phy_cor") {
-    #x <- t(chol(C_true)) %*% rnorm(nrow(comm),x_mean,x_sd)
-      x <- mvrnorm(1,rep(0,nrow(C_true)),C_true)
+    spaMM_formula <- as.formula(spaMM_formula)
+
+    models <- CPR_spaMM(spaMM_formula,
+                        data=sim_data$data,
+                        VCV_sp = sim_data$sim_phy,
+                        true_VCV = sim_data$true_phy,
+                        comm=comm,
+                        family= "gaussian",
+                        optim.lambda = T,
+                        control.HLfit = list(max.iter=10000),
+                        control.optim=list(factr=1e15))
+
+    conv <- models$conv
     }
-
-    if (signals_X == "sr") {
-      x <- specnumber(comm)
-    }
-
-    if (signals_X == "no_phy_cor") {
-      x <- rnorm(nrow(comm),x_mean,x_sd)
-    }
-
-    if (signals_Y == T) {
-    #y <- t(chol(C_true)) %*% rnorm(nrow(comm),y_mean,y_sd)
-      y <- intercept+b1*x+mvrnorm(1,rep(0,nrow(C_true)),C_true)+rnorm(nrow(comm),noise_mean,noise_sd)
-    } else {
-      y <- intercept+b1*x+rnorm(nrow(comm),y_mean,y_sd)+rnorm(nrow(comm),noise_mean,noise_sd)
-    }
-
-    sim_all[[i]] <- data.frame(y=y,x=x)
-  }
-
-
-  sim_dat <- list(sim_dat = sim_all,
-                  sim_phy = VCV_sp_list,
-                  true_phy = VCV_true_list)
-
-  return(sim_dat)
+    return(models)
+  # }
 }
+
+
--- R/CPR_spaMM.R
+++ R/CPR_spaMM.R
@@ -1,19 +1,23 @@
 CPR_spaMM <- function(formula,
-                    data,
-                    VCV_sp,
-                    comm,
-                    optim.lambda=T,
-                    original.VCV=T,
-                    AIC_threshold = -4,
-                    init=list(lambda=NA),
-                    method.spaMM = "REML",
-                    ...) {
+                      data,
+                      VCV_sp,
+                      comm,
+                      optim.lambda=T,
+                      original.VCV=T,
+                      AIC_threshold = -4,
+                      init=list(lambda=NA),
+                      method.spaMM = "REML",
+                      true_VCV = NULL,
+                      control.optim=NULL,
+                      ...) {
 
   require(nlme)
   require(NMOF)
   require(spaMM)
+  require(admisc)
 
   message("modelling")
+  .env <- new.env()
   data$comp_id <- 1:nrow(data)
 
   formula_text <- Reduce(paste,deparse(formula))
@@ -28,12 +32,14 @@ CPR_spaMM <- function(formula,
   AIC_without_phylo <- AIC(m_without_comp,verbose=F)[[1]]
 
   lambda_spaMM <- NA
-  AIC_optim <- NA
+  AIC_optim <- AIC_true <- NA
   optimized_model_result <- NA
   best_m <- m_optim <- NA
 
   C.lambda.spaMM <- get_comm_pair_r(comm,VCV_sp,force.PD=F)$covM
-  rownames(C.lambda.spaMM) <- data$comp_id
+  rownames(C.lambda.spaMM) <- as.character(data$comp_id)
+  C.lambda.spaMM <- C.lambda.spaMM #seems the package only extracts vars from the global environment...
+
   m_original_VCV <- fitme(formula,
                           corrMatrix=as_precision(C.lambda.spaMM),
                           data=data,
@@ -46,7 +52,9 @@ CPR_spaMM <- function(formula,
   diag(VCV_sp_lambda0) <- diag(VCV_sp)
 
   C.lambda0.spaMM <- get_comm_pair_r(comm,VCV_sp_lambda0,force.PD=F)$covM
-  rownames(C.lambda0.spaMM) <- data$comp_id
+  rownames(C.lambda0.spaMM) <- as.character(data$comp_id)
+  C.lambda0.spaMM <- C.lambda0.spaMM #seems the package only extracts vars from the global environment...
+
   m_lambda0 <- fitme(formula,
                    corrMatrix=as_precision(C.lambda0.spaMM),
                    data=data,
@@ -54,6 +62,20 @@ CPR_spaMM <- function(formula,
                    method=method.spaMM,
                    ...)
   AIC_star <- AIC(m_lambda0,verbose=F)[[1]]
+
+  if (!is.null(true_VCV)) {
+    C.true <- get_comm_pair_r(comm,true_VCV,force.PD=F)$covM
+    rownames(C.true) <- as.character(data$comp_id)
+    C.true <- C.true #seems the package only extracts vars from the global environment...
+
+    m_true <- fitme(formula,
+                    corrMatrix=as_precision(C.true),
+                    data=data,
+                    init=init,
+                    method=method.spaMM,
+                    ...)
+    AIC_true <- AIC(m_true,verbose=F)[[1]]
+  }
 
   if (optim.lambda == T) {
 
@@ -68,6 +90,7 @@ CPR_spaMM <- function(formula,
                               printDetail = F,
                               method.spaMM=method.spaMM,
                               init=init,
+                              control=control.optim,
                               ...)
 
     ML.opt<-optim(grid_result$minlevels,
@@ -81,19 +104,21 @@ CPR_spaMM <- function(formula,
                   upper=1,
                   init=init,
                   method.spaMM = method.spaMM,
+                  control=control.optim,
                   ...)
 
     lambda_spaMM<-ML.opt$par
     logL<--ML.opt$value
     VCV_sp_optim <- VCV_sp*lambda_spaMM
     diag(VCV_sp_optim) <- diag(VCV_sp)
-    C.lambda.spaMM<- get_comm_pair_r(comm,VCV_sp_optim,force.PD=F)$covM
-    rownames(C.lambda.spaMM) <- data$comp_id
+    C.lambda.optim.spaMM <- get_comm_pair_r(comm,VCV_sp_optim,force.PD=F)$covM
+    rownames(C.lambda.optim.spaMM) <- as.character(data$comp_id)
+    C.lambda.optim.spaMM <- C.lambda.optim.spaMM #seems the package only extracts vars from the global environment...
 
     m_optim <- fitme(formula,
-                     corrMatrix=as_precision(C.lambda.spaMM),
+                     corrMatrix=as_precision(C.lambda.optim.spaMM),
                      data=data,
-                     method.spaMM=method.spaMM,
+                     method=method.spaMM,
                      init=init,
                      ...)
 
@@ -118,14 +143,54 @@ CPR_spaMM <- function(formula,
     }
   }
 
-  output <- list(best_m = best_m,
+  ##########drop 1
+   .drop1_spamm <- function(model,C) {
+     conv <- 0
+     model$call$corrMatrix <- as_precision(C)
+     model$call$method <- ifelse(method.spaMM == "REML","REML","ML")
+     result_satt <- drop1(model,verbose=F)
+
+     msg <- tryCatchWEM(drop1(model,verbose=F))$warning
+
+     if (is.null(msg$warning)) {
+       msg$warning <- "OK"
+     }
+
+     if (!grepl("converge",msg$warning)) conv <- 1
+
+     result <- list(result=result_satt,
+                    msg=msg,
+                    conv=conv)
+     return(result)
+   }
+
+   best_model_satt <- .drop1_spamm(best_m,get(gsub("as_precision(*)","",best_m$call$corrMatrix)[[2]]))
+   original_VCV_model_satt <- .drop1_spamm(m_original_VCV,C.lambda.spaMM)
+   optim_model_satt <- .drop1_spamm(m_optim,C.lambda.optim.spaMM)
+   true_model_satt <- .drop1_spamm(m_true,C.true)
+  # best_model_satt <- drop1(best_m)
+  # optim_model_satt <- drop1(m_optim)
+  # original_VCV_model_satt <- drop1(m_original_VCV)
+  # true_model_satt <- drop1(m_true)
+
+   output <- list(best_model = best_m,
+                 best_model_satt = best_model_satt$result,
                  optimized_lambda_model = m_optim,
-                 without_comp_model = m_without_comp,
+                 optimized_lambda_model_satt = optim_model_satt$result,
                  original_VCV_model = m_original_VCV,
+                 original_VCV_m_satt = original_VCV_model_satt$result,
+                 true_model = m_true,
+                 true_model_satt = true_model_satt$result,
+                 without_comp_model = m_without_comp,
                  star_model = m_lambda0,
                  AIC = c(AIC_without_phylo =  AIC_without_phylo,
                          AIC_original_VCV =  AIC_original_VCV,
                          AIC_optim = AIC_optim,
-                         AIC_star = AIC_star),
-                 optimized_lambda = lambda_spaMM)
+                         AIC_star = AIC_star,
+                         AIC_true = AIC_true),
+                 optimized_lambda = lambda_spaMM,
+                 conv = c(best_m=best_model_satt$conv,
+                             optim_m=optim_model_satt$conv,
+                             orig_m=original_VCV_model_satt$conv,
+                             true_m=true_model_satt$conv))
 }
